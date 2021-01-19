53a11a86cbf486e54f554585cbac7af9d5d28566
Haliam <haliampf@gmail.com>
2021-01-19 11:05:57 +0000

    Enable-Migrations Add -Migration FirstMigration Update-Database

---
+++ Migrations/202101191050341_InitialCreate.cs
@@ -0,0 +1,31 @@
+namespace TelerikMvcTraining.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductID = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(),
+                        SupplierID = c.Int(),
+                        CategoryID = c.Int(),
+                        QuantityPerUnit = c.String(),
+                        UnitPrice = c.Decimal(precision: 18, scale: 2),
+                        Discontinued = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
