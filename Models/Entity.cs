06fc9ff76ea14f28a41f7661423a08411c1cff22
Haliam <haliampf@gmail.com>
2021-01-19 10:38:29 +0000

    Add first classes

---
+++ Models/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelerikMvcTraining.Models
+{
+    public abstract class Entity 
+    {
+    }
+}
