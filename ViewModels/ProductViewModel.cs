5033b4b30a6600df884d7279b089b78ed98b2c83
Haliam <haliampf@gmail.com>
2021-01-19 11:27:58 +0000

    Add ViewModels.

---
+++ ViewModels/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TelerikMvcTraining.Models;
+
+namespace TelerikMvcTraining.ViewModels
+{
+    public class ProductViewModel : ViewModel<Product>
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int? SupplierID { get; set; }
+        public int? CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public uint? UnitsInStock { get; set; }
+        public uint? UnitsOnOrder { get; set; }
+        public uint? ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+    }
+}
---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TelerikMvcTraining.Models;
+
+namespace TelerikMvcTraining.ViewModels
+{
+    public abstract class ViewModel<TEntity> where TEntity : Entity
+    {
+    }
+}
