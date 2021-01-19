97e7d09a59823a9b6c89c34ec9a5acffa3a2db0c
Haliam <haliampf@gmail.com>
2021-01-19 17:32:33 +0000

    Add Repository Pattern

---
+++ Data/Repositories/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TelerikMvcTraining.Models;
+
+namespace TelerikMvcTraining.Data.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(TelerikContext dbContext) : base(dbContext)
+        {
+
+        }
+
+        public Product GetEntity(int id)
+        {
+            return Get(m => m.ProductID == id, null, "Product").FirstOrDefault();
+        }
+    }
+}
---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using TelerikMvcTraining.Data.Repositories;
+
+namespace TelerikMvcTraining.Data.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+
+        private readonly TelerikContext DbContext;
+        private readonly DbSet<TEntity> dbSet;
+
+        public Repository(TelerikContext dbContext)
+        {
+            DbContext = dbContext;
+            dbSet = dbContext.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null, 
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null, 
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public TEntity GetByID(object id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public void Insert(TEntity entity)
+        {
+             dbSet.Add(entity);
+        }
+
+        public void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            DbContext.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public void Delete(object id)
+        {
+              TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public void Delete(TEntity entityToDelete)
+        {
+            if (DbContext.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+    }
+}
