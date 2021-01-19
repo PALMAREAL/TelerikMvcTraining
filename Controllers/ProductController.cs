e0704edfe9635395adac6b15a31c02db677b53db
Haliam <haliampf@gmail.com>
2021-01-19 16:22:56 +0000

    Add BasicGrid View

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -9,6 +9,9 @@ using System.Web;
 using System.Web.Mvc;
 using TelerikMvcTraining.Data;
 using TelerikMvcTraining.Models;
+using Kendo.Mvc.UI;
+using Kendo.Mvc.Extensions;
+using TelerikMvcTraining.ViewModels;
 
 namespace TelerikMvcTraining.Controllers
 {
@@ -16,106 +19,32 @@ namespace TelerikMvcTraining.Controllers
     {
         private TelerikContext db = new TelerikContext();
 
-        // GET: Product
-        public async Task<ActionResult> Index()
-        {
-            return View(await db.Products.ToListAsync());
-        }
 
-        // GET: Product/Details/5
-        public async Task<ActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = await db.Products.FindAsync(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // GET: Product/Create
-        public ActionResult Create()
+        public ActionResult BasicGrid()
         {
             return View();
         }
 
-        // POST: Product/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,Discontinued")] Product product)
+        public ActionResult Products_Read([DataSourceRequest] DataSourceRequest request)
         {
-            if (ModelState.IsValid)
-            {
-                db.Products.Add(product);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
 
-            return View(product);
+            var result = Enumerable.Range(0, 50).Select(i => new ProductViewModel
+            {
+                ProductID = i,
+                SupplierID = i + 1,
+                ProductName = "SamsungTablet" + "  A." + (i*2),
+                UnitPrice = i + 25,
+                Discontinued = false,
+                CategoryID = i + 2,
+                QuantityPerUnit = (i + 10).ToString(),
+                UnitsInStock = (uint?)(i + 3),
+                UnitsOnOrder = (uint?)(i + 4),
+                ReorderLevel = (uint?)(i + 20)
+            });
+
+            return Json(result.ToDataSourceResult(request));
         }
 
-        // GET: Product/Edit/5
-        public async Task<ActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = await db.Products.FindAsync(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Product/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,Discontinued")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(product).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(product);
-        }
-
-        // GET: Product/Delete/5
-        public async Task<ActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Product product = await db.Products.FindAsync(id);
-            if (product == null)
-            {
-                return HttpNotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Product/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
-        {
-            Product product = await db.Products.FindAsync(id);
-            db.Products.Remove(product);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
 
         protected override void Dispose(bool disposing)
         {
--- ViewModels/ProductViewModel.cs
+++ ViewModels/ProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using TelerikMvcTraining.Models;
@@ -9,14 +10,33 @@ namespace TelerikMvcTraining.ViewModels
     public class ProductViewModel : ViewModel<Product>
     {
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
+        [StringLength(50, MinimumLength = 1, ErrorMessage = " The Name should be a maximum of 50 characters")]
+        [Display(Name = "Name")]
         public string ProductName { get; set; }
+
+        [Display(Name = "Supplier")]
         public int? SupplierID { get; set; }
+
+        [Display(Name = "Category")]
         public int? CategoryID { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
         public string QuantityPerUnit { get; set; }
+
+        [Display(Name = "Price")]
         public decimal? UnitPrice { get; set; }
+
+        [Display(Name = "In Stock")]
         public uint? UnitsInStock { get; set; }
+
+        [Display(Name = "Units on order")]
         public uint? UnitsOnOrder { get; set; }
+
         public uint? ReorderLevel { get; set; }
+
+        [Required(ErrorMessage = "{0} is required.")]
         public bool Discontinued { get; set; }
     }
 }
