86a18564a3ed5a535121b425b1e3bfcb5c27d621
Haliam <haliampf@gmail.com>
2021-01-20 14:15:47 +0000

    Add Basic and Advanced Chart

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -16,105 +16,19 @@ namespace TelerikMvcTraining.Controllers
     {
         private TelerikContext db = new TelerikContext();
 
-        // GET: Customer
-        public async Task<ActionResult> Index()
-        {
-            return View(await db.Customers.ToListAsync());
-        }
-
-        // GET: Customer/Details/5
-        public async Task<ActionResult> Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = await db.Customers.FindAsync(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // GET: Customer/Create
-        public ActionResult Create()
+        public ActionResult BasicChart()
         {
             return View();
         }
 
-        // POST: Customer/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
+        public ActionResult AdvancedChart()
         {
-            if (ModelState.IsValid)
-            {
-                db.Customers.Add(customer);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-
-            return View(customer);
+            return View();
         }
 
-        // GET: Customer/Edit/5
-        public async Task<ActionResult> Edit(string id)
+        public ActionResult ProChart()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = await db.Customers.FindAsync(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // POST: Customer/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "CustomerID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax")] Customer customer)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(customer).State = EntityState.Modified;
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
-            return View(customer);
-        }
-
-        // GET: Customer/Delete/5
-        public async Task<ActionResult> Delete(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Customer customer = await db.Customers.FindAsync(id);
-            if (customer == null)
-            {
-                return HttpNotFound();
-            }
-            return View(customer);
-        }
-
-        // POST: Customer/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(string id)
-        {
-            Customer customer = await db.Customers.FindAsync(id);
-            db.Customers.Remove(customer);
-            await db.SaveChangesAsync();
-            return RedirectToAction("Index");
+            return View();
         }
 
         protected override void Dispose(bool disposing)
