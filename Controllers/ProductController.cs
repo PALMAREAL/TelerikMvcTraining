52647e096d411472b21a1cdb87594834279176f3
Haliam <haliampf@gmail.com>
2021-02-03 13:24:40 +0000

    Implementing AutoMapper in ProductController

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -8,63 +8,95 @@ using TelerikMvcTraining.ViewModels;
 using System.Linq;
 using System;
 using TelerikMvcTraining.Services;
+using AutoMapper;
+using System.Collections.Generic;
 
 namespace TelerikMvcTraining.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : BaseController
     {
-        //public IProductRepository ProductRepository { get; set; }
-
         public IProductService ProductService { get; set; }
 
-        public ProductController(IProductService productService)
+        public ProductController(IProductService productService, IMapper mapper) : base (mapper)
         {
             ProductService = productService;
-        }   
+        }
 
         public ActionResult Products_Read([DataSourceRequest] DataSourceRequest request)
         {
-            return Json(ProductService.GetAllProducts().ToDataSourceResult(request));
+            var products = ProductService.GetAllProducts();
+
+            var productViewModel = Mapper.Map<IEnumerable<ProductViewModel>>(products);
+
+            return Json(productViewModel.ToDataSourceResult(request));
         }
 
         public ActionResult DetailProducts_Read([DataSourceRequest] DataSourceRequest request)
         {
-            return Json(ProductService.GetAllDetailsProducts().ToDataSourceResult(request));
+            var products = ProductService.GetAllDetailsProducts();
+
+            var detailProductViewModel = Mapper.Map<IEnumerable<DetailProductViewModel>>(products);
+
+            return Json(detailProductViewModel.ToDataSourceResult(request));
         }
 
         [HttpPost]
         public ActionResult SalesByProductCategory()
         {
-            return Json(ProductService.GetProductCategoriesData());
+            var products = ProductService.GetProductCategoriesData();
+
+            var productCategoriesViewModel = Mapper.Map<IEnumerable<ProductCategoriesViewModel>>(products);
+
+            return Json(productCategoriesViewModel);
         }
 
         [HttpPost]
         public ActionResult FunnelSales()
         {
-            return Json(ProductService.GetFunnelSalesData());
+            var products = ProductService.GetFunnelSalesData();
+
+            var funnelSalesViewModel = Mapper.Map<IEnumerable<FunnelSalesViewModel>>(products);
+
+            return Json(funnelSalesViewModel);
         }
 
         [HttpPost]
         public ActionResult SalesByRegion()
         {
-            return Json(ProductService.GetRegionSalesData());
+            var products = ProductService.GetRegionSalesData();
+
+            var regionSalesViewModel = Mapper.Map<IEnumerable<RegionSalesViewModel>>(products);
+
+            return Json(regionSalesViewModel);
         }
 
         [HttpPost]
         public ActionResult SalesPerformers()
         {
-            return Json(ProductService.GetSalesPerformers());
+            var products = ProductService.GetSalesPerformers();
+
+            var salesPerformersViewModel = Mapper.Map<IEnumerable<SalesPerformersViewModel>>(products);
+
+            return Json(salesPerformersViewModel);
         }
 
         [HttpPost]
         public ActionResult BoeingStockDataRead()
         {
-            return Json(ProductService.BoeingStockData());
+            var products = ProductService.BoeingStockData();
+
+            var stockDataPointViewModel = Mapper.Map<IEnumerable<StockDataPointViewModel>>(products);
+
+            return Json(stockDataPointViewModel);
         }
 
         public JsonResult Get_Categories()
         {
-            return Json(ProductService.GetCategoriesData(), JsonRequestBehavior.AllowGet);
+            var products = ProductService.GetCategoriesData();
+
+            var categoryViewModel = Mapper.Map<IEnumerable<CategoryViewModel>>(products);
+
+            return Json(categoryViewModel, JsonRequestBehavior.AllowGet);
         }
     }
 }
