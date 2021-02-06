5b2c76072c9ba31962c033e8fe5efe7a4ac4f20f
Haliam <haliampf@gmail.com>
2021-02-06 13:13:34 +0000

    Add Inheritance from Base Controller

--- Controllers/DropdownListController.cs
+++ Controllers/DropdownListController.cs
@@ -1,13 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TelerikMvcTraining.Services;
+using TelerikMvcTraining.ViewModels;
 
 namespace TelerikMvcTraining.Controllers
 {
-    public class DropdownListController : Controller
+    public class DropdownListController : BaseController
     {
+        public IProductService ProductService { get; set; }
+
+        public DropdownListController(IProductService productService, IMapper mapper) : base(mapper)
+        {
+            ProductService = productService;
+        }
+
         public ActionResult BasicDropDownList()
         {
             return View();
@@ -17,5 +29,14 @@ namespace TelerikMvcTraining.Controllers
         {
             return View();
         }
+
+        public ActionResult Detail_Products_Read()
+        {
+            var products = ProductService.GetAllDetailsProducts();
+
+            var detailProductViewModel = Mapper.Map<IEnumerable<DetailProductViewModel>>(products);
+
+            return Json(detailProductViewModel, JsonRequestBehavior.AllowGet);
+        }        
     }
 }
