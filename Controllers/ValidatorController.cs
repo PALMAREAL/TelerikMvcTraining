86fe4b695ca8efcf28e914e13a5efc9995325917
Haliam <haliampf@gmail.com>
2021-02-09 10:09:46 +0000

    Add Validator component.

---
+++ Controllers/ValidatorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelerikMvcTraining.Controllers
+{
+    public class ValidatorController : Controller
+    {
+        public ActionResult BasicValidator()
+        {
+            return View();
+        }
+
+    }
+}
